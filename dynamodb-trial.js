97551f6a0bdbf190a6f94df88fec66c60af4cac7
clavius <16825879+ja-martinez@users.noreply.github.com>
2022-11-12 11:46:34 -0700

    implement database query

--- dynamodb-trial.js
+++ dynamodb-trial.js
@@ -9,10 +9,12 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import {
   DynamoDBDocumentClient,
   BatchWriteCommand,
+  QueryCommand
 } from "@aws-sdk/lib-dynamodb";
 
 const REGION = "us-east-2";
 const TABLE_NAME = "Jobs";
+const COMPANY_NAME = "schrodinger"
 
 const ddbClient = new DynamoDBClient({ region: REGION });
 const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);
@@ -66,9 +68,30 @@ async function updateDB(newJobs, removedJobs) {
 
   try {
     const data = await ddbDocClient.send(new BatchWriteCommand(params));
-    console.log(data);
+    console.log("Success. Database updated", data);
   } catch (err) {
     console.error(err.stack)
+  }
+}
+
+async function getPrevJobs() {
+  const params = {
+    TableName: TABLE_NAME,
+    ExpressionAttributeValues: {
+      ":c": COMPANY_NAME
+    },
+    KeyConditionExpression: "companyName = :c"
+  }
+
+  const jobs = {};
+  try {
+    const data = await ddbDocClient.send(new QueryCommand(params));
+    const jobsArray = data.Items
+    jobsArray.forEach((job) => jobs[job.jobID] = job);
+    console.log("success. Item Details: ", jobs)
+    return jobs;
+  } catch (err) {
+    throw new Error(err);
   }
 }
 
@@ -97,3 +120,5 @@ async function updateDB(newJobs, removedJobs) {
 // ]
 
 // updateDB(newJobs, removedJobs);
+
+getPrevJobs()
