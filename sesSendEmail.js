cd12b609e3a6e24f3f690922298b8b6f524b7036
clavius <16825879+ja-martinez@users.noreply.github.com>
2022-11-16 12:39:42 -0700

    fix email styling

--- sesSendEmail.js
+++ sesSendEmail.js
@@ -3,43 +3,45 @@ Function that will accept array. Array will only contain companies that changed 
 
 TODO: add from_address and to_address to environment
 */
-import { renderFile, render } from "ejs";
+import { renderFile } from "ejs";
 import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 
 const REGION = "us-east-2";
 
-const FROM_ADDRESS = "alejandro@clavius.dev";
+const FROM_ADDRESS = "job-notifications@clavius.dev";
 const TO_ADDRESS = "j.alejandro.mrtnz@gmail.com";
 const EMAIL_SUBJECT = "Today's Job Updates";
 const EMAIL_TEMPLATE_FILENAME = "./email-template.ejs";
 
-const sesClient = new SESClient({ region: REGION });
-const sendEmailCommand = new SendEmailCommand({
-  Destination: {
-    ToAddresses: [TO_ADDRESS],
-  },
-  Message: {
-    /* required */
-    Body: {
-      /* required */
-      Html: {
-        Charset: "UTF-8",
-        Data: "HTML_FORMAT_BODY", // TODO: function to create HTML body
+function createSendEmailCommand(
+  toAddress,
+  fromAddress,
+  emailSubject,
+  htmlBody
+) {
+  return new SendEmailCommand({
+    Destination: {
+      ToAddresses: [toAddress],
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          Data: htmlBody,
+        },
       },
-      Text: {
+      Subject: {
         Charset: "UTF-8",
-        Data: "TEXT_FORMAT_BODY", // TODO: function to create test body
+        Data: emailSubject,
       },
     },
-    Subject: {
-      Charset: "UTF-8",
-      Data: EMAIL_SUBJECT,
-    },
-  },
-  Source: FROM_ADDRESS,
-});
+    Source: fromAddress,
+  });
+}
 
-async function getHtmlBody(companies) {
+async function getHtmlBody(companies, emailTemplateFilename) {
+  // returns HTML string from companies array
+
   const renderFileWithPromise = (fileName, data, options) => {
     return new Promise((resolve, reject) => {
       renderFile(fileName, data, options, (err, str) => {
@@ -50,58 +52,74 @@ async function getHtmlBody(companies) {
   };
 
   const htmlString = await renderFileWithPromise(
-    EMAIL_TEMPLATE_FILENAME,
+    emailTemplateFilename,
     { companies },
     { rmWhiteSpace: true }
   );
 
-  console.log(htmlString);
-
   return htmlString;
 }
 
+async function sendEmail(companies) {
+  const sesClient = new SESClient({ region: REGION });
+  const htmlBody = await getHtmlBody(companies, EMAIL_TEMPLATE_FILENAME);
+  const sendEmailCommand = createSendEmailCommand(
+    TO_ADDRESS,
+    FROM_ADDRESS,
+    EMAIL_SUBJECT,
+    htmlBody
+  );
+
+  try {
+    return await sesClient.send(sendEmailCommand);
+  } catch (e) {
+    console.error("failed to send email.");
+    return e;
+  }
+}
 
 // Testing
 const companies = [
   {
-    companyName: 'Schrodinger',
+    companyName: "Schrodinger",
     newJobs: [
       {
         title: "boosdf",
         locations: "nyc, ny; tampa, FL",
-        url: "google.com"
-      }
+        url: "google.com",
+      },
     ],
     removedJobs: [
       {
         title: "aaaa",
         locations: "aaa, ny; taaaa FL",
-        url: "google.com"
-      }
-    ]
+        url: "google.com",
+      },
+    ],
   },
   {
-    companyName: 'Google',
+    companyName: "Google",
     newJobs: [
       {
         title: "boossdf",
         locations: "nyc, ny; tampa, FL",
-        url: "google.com"
+        url: "google.com",
       },
       {
         title: "bsdf",
         locations: "nyc, ny",
-        url: "google.com"
-      }
+        url: "google.com",
+      },
     ],
     removedJobs: [
       {
         title: "aaaa",
         locations: "aaa, ny; taaaa FL",
-        url: "google.com"
-      }
-    ]
-  }
-]
+        url: "google.com",
+      },
+    ],
+  },
+];
 
-getHtmlBody(companies)
+// getHtmlBody(companies, EMAIL_TEMPLATE_FILENAME);
+sendEmail(companies);
